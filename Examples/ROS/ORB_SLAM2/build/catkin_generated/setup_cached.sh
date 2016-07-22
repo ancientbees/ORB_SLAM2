544a357a2c02986cfb784115ac5620c59f957ef8
James Garforth <jamesgarforth@gmail.com>
2016-07-22 13:20:09 +0100

    ORB-SLAM set up for my machine, should be no code changes only config

---
+++ Examples/ROS/ORB_SLAM2/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jamesgarforth/Documents/ORB_SLAM2/Examples/ROS/ORB_SLAM2/build/devel:$CMAKE_PREFIX_PATH"
+export PYTHONPATH="/home/jamesgarforth/catkin_ws/devel/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:/home/jamesgarforth/bebop_ws/devel/lib/python2.7/dist-packages"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jamesgarforth/Documents/ORB_SLAM2/Examples/ROS/ORB_SLAM2/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jamesgarforth/Documents/ORB_SLAM2/Examples/ROS/ORB_SLAM2:/home/jamesgarforth/bebop_ws/src:/home/jamesgarforth/catkin_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
